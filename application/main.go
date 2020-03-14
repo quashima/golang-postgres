5db65d18805200b3bd5294b5dca4befe29077861
quashima <ex.rwksm1024@gmail.com>
2020-03-14 23:38:29 +0900

    add postgres driver ; "

--- application/main.go
+++ application/main.go
@@ -1,7 +1,21 @@
 package main
 
 import "fmt"
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
 
 func main() {
-	fmt.Println("Hello, 世界")
+	connStr := ""
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	age := 21
+	rows, err := db.Query("SELECT name FROM users WHERE age = $1", age)
+	
 }
