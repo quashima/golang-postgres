4a1eabd829eaef3b341613b5813be8a8e90718ad
quashima <ex.rwksm1024@gmail.com>
2020-03-15 01:25:49 +0900

    add config

--- application/main.go
+++ application/main.go
@@ -1,21 +1,30 @@
 package main
 
-import "fmt"
 import (
+	"gopkg.in/ini.v1"
 	"database/sql"
+	"log"
 
 	_ "github.com/lib/pq"
 )
 
+var Config ConfigList
+
+func init() {
+	cfg, _ := ini.load("config.ini")
+	Config = ConfigList{
+		Url:    cfg.Section("db").key("url").MustString(),
+		Driver: cfg.Section("db").key("driver").String(),
+	}
+}
 
 func main() {
-	connStr := ""
-	db, err := sql.Open("postgres", connStr)
+	connUrl := Config.Url
+	db, err := sql.Open(Config.Driver, connUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	age := 21
 	rows, err := db.Query("SELECT name FROM users WHERE age = $1", age)
-	
 }
