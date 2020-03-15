68323b7c20d52d34ebd6ead96ed2851b8cd2a00c
quashima <ex.rwksm1024@gmail.com>
2020-03-16 00:15:42 +0900

    change ini method to uppercase

--- application/main.go
+++ application/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	_ "github.com/lib/pq"
+	"gopkg.in/ini.v1"
 )
 
 type ConfigList struct {
@@ -15,25 +16,35 @@ type ConfigList struct {
 	SslMode string
 }
 
+type User struct {
+	Id string
+}
+
 var Config ConfigList
 
 func init() {
-	cfg, _ := ini.load("config.ini")
+	cfg, _ := ini.Load("../config.ini")
 	Config = ConfigList{
-		Driver:  cfg.Section("db").key("driver").String(),
-		User:    cfg.Section("db").key("user").String(),
-		DbName:  cfg.Section("db").key("dbname").String(),
-		SslMode: cfg.Section("db").key("sslmode").String(),
+		Driver:  cfg.Section("db").Key("driver").String(),
+		User:    cfg.Section("db").Key("user").String(),
+		DbName:  cfg.Section("db").Key("dbname").String(),
+		SslMode: cfg.Section("db").Key("sslmode").String(),
 	}
 }
 
 func main() {
-	connStr := "user=" + Config.User + "dbname=" + Config.DbName + "sslmode=" + Config.SslMode
+	connStr := "user=" + Config.User + " dbname=" + Config.DbName + " sslmode=" + Config.SslMode
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	rows, err := db.Query("select * from user")
-	fmt.Printf("%v", rows)
+	var users []User
+	for rows.Next() {
+		var user User
+		rows.Scan(&user.Id)
+		users = append(users, user)
+	}
+	fmt.Printf("%v", users)
 }
