e7809838ec2f9a96ee5d72ef28c8ca49613f4304
quashima <ex.rwksm1024@gmail.com>
2020-03-22 22:52:04 +0900

    create new row.go

---
+++ interface/controller/row.go
@@ -0,0 +1,20 @@
+package row
+
+import (
+	"database/sql"
+
+	usertable "../../domain"
+)
+
+func RowToArrayMap(rows *sql.Rows) map[int]*usertable.Column {
+	var usersMap = map[int]*usertable.Column{}
+	var count = 0
+	for rows.Next() {
+		var user usertable.Column
+		rows.Scan(&user.Id)
+
+		usersMap[count] = &usertable.Column{Id: user.Id}
+		count++
+	}
+	return usersMap
+}
--- main.go
+++ main.go
@@ -1,40 +1,17 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	usertable "./domain"
 	dbconnection "./infrastructure"
+	row "./interface/controller"
 
 	_ "github.com/lib/pq"
 )
 
-type ConfigList struct {
-	Driver  string
-	User    string
-	DbName  string
-	SslMode string
-}
-
-var Config ConfigList
-
-func arrayMap(rows *sql.Rows) map[int]*usertable.Column {
-	var usersMap = map[int]*usertable.Column{}
-	var count = 0
-	for rows.Next() {
-		var user usertable.Column
-		rows.Scan(&user.Id)
-
-		usersMap[count] = &usertable.Column{Id: user.Id}
-		count++
-	}
-	return usersMap
-}
-
 func main() {
 	rows := dbconnection.Connection()
-	resultsMap := arrayMap(rows)
+	resultsMap := row.RowToArrayMap(rows)
 
 	fmt.Printf("%s", resultsMap[0].Id)
 }
